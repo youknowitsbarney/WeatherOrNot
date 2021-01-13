ff4b880c54d83a1f0759500e151d12364039383e
Barney <barneym29@gmail.com>
2021-01-13 12:29:10 +0300

    Initial Commit

---
+++ WeatherOrNot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherOrNot
+//
+//  Created by Barney on 13/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
