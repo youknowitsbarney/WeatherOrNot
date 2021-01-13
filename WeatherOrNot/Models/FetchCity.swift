da116e549644714ed9620e6050d7859078d8dc55
Barney <barneym29@gmail.com>
2021-01-13 13:48:32 +0300

    FetchCity Model

--- WeatherOrNot/Models/FetchCity.swift
+++ WeatherOrNot/Models/FetchCity.swift
@@ -6,3 +6,62 @@
 //
 
 import Foundation
+
+//MARK: Fetch single city response
+
+struct FetchCity: Codable {
+    
+    let coord: Coordinates
+    let weather: [Weather]
+    let base: String
+    let main: Main
+    let visibility: Int
+    let wind: Wind
+    let clouds: Clouds
+    let dt: Int
+    let sys: Sys
+    let timezone, id: Int
+    let name: String
+    let cod: Int
+}
+
+//MARK: Coordinates
+struct Coordinates: Codable {
+    
+    let longitude, latitude: Double
+}
+
+// MARK: Weather
+struct Weather: Codable {
+    
+    let id: Int
+    let main, description: String
+    let icon: String
+}
+
+// MARK: Main
+struct Main: Codable {
+    
+    let temp, feelsLike, min, max: Double
+    let pressure, humidity: Int
+}
+
+// MARK: Wind
+struct Wind: Codable {
+    
+    let speed: Double
+    let degrees: Int
+}
+
+// MARK: Clouds
+struct Clouds: Codable  {
+    
+    let all: Int
+}
+
+// MARK: Sys
+struct Sys: Codable {
+    
+    let type, id, sunrise, sunset: Int
+    let country: String
+}
